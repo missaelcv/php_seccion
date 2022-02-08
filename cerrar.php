e2823c1e6416084da4be8ef0e7052aa3ebcc5c3e
missaelcv <missaelcv0220@gmail.com>
2022-02-07 22:26:54 -0400

    detroy la session de la pagina web

--- cerrar.php
+++ cerrar.php
@@ -2,4 +2,15 @@
 
 session_start();
 
+$_SESSION = array();
+
+if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '',time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"],$params["httponly"]
+                );
+}
+
+session_destroy();
 ?>
