458df0ea01f696b6ee94cf091ee9d32599e815a4
missaelcv <missaelcv0220@gmail.com>
2022-02-07 22:05:01 -0400

    la ['admin']se coloco de administrador

--- protegido.php
+++ protegido.php
@@ -2,3 +2,10 @@
 
 session_start();
 
+if (isset($_SESSION['admin']) ){
+      echo 'Bienvenido! '.$_SESSION['admin'];
+      echo '<br><a href="cerrar.php">Cerrar Sesion</a>';
+      
+}else {
+    header('Location:index.php');
+}
