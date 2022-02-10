6c071edaecd4024913e8b3729f9234e90cbec9c5
missaelcv <missaelcv0220@gmail.com>
2022-02-09 23:06:48 -0400

    hasd de contrasena

--- form-sesion/agregar_usuario.php
+++ form-sesion/agregar_usuario.php
@@ -1,5 +1,24 @@
 <?php
 
-echo password_hash("rasmuslerdorf", PASSWORD_DEFAULT)."\n";
+//echo password_hash("missael", PASSWORD_DEFAULT)."\n";
 
+$usuario_nuevo = $_POST['nombre_usuario'];
+$contrasena = $_POST['constrasena'];
+$contrasena2 = $_POST['constrasena2'];
+
+//hash de contrasena para validar 
+$contrasena = password_hash("missael", PASSWORD_DEFAULT)."\n";
+
+echo '<pre>';
+var_dump($usuario_nuevo);
+var_dump($contrasena);
+var_dump($contrasena2);
+echo '<pre>';
+
+
+if (password_verify($constrasena2, $constrasena)) {
+    echo '¡La contraseña es válida!';
+} else {
+    echo 'La contraseña no es válida.';
+}
 ?>
