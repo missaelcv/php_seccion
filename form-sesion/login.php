1d2c949e7087f4102a01683f9b7de68da4174c5a
missaelcv <missaelcv0220@gmail.com>
2022-02-11 23:24:02 -0400

    se creo login.php

---
+++ form-sesion/login.php
@@ -0,0 +1,11 @@
+<?php
+
+$usuario_login = $_POST['nombre_usuario'];
+$contrasena_login = $_POST['contrasena'];
+
+echo '<pre>';
+var_dump($usuario_login);
+var_dump($contrasena_login);
+echo '<pre>';
+
+
