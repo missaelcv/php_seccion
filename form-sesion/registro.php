92671e2e3f18cfde9b8cde6e4fc723806f9c255e
missaelcv <missaelcv0220@gmail.com>
2022-02-11 23:24:48 -0400

    creando el registro login.php

--- form-sesion/registro.php
+++ form-sesion/registro.php
@@ -12,10 +12,18 @@ session_start();
     <title>Form-Sesion.php</title>
 </head>
 <body>
+    <h3>Registro Usuario</h3>
     <form action="agregar_usuario.php" method="POST">
         <input type="text" name="nombre_usuario" placeholder="Ingrese Usuario">
         <input type="text" name="contrasena" placeholder="Ingrese Contrasena">
         <input type="text" name="contrasena2" placeholder="Ingrese nuevamente pass">
+        <button type="submit">Guardar</button>
+    </form>
+
+    <h3>Login</h3>
+    <form action="login.php" method="POST">
+        <input type="text" name="nombre_usuario" placeholder="Ingrese Usuario">
+        <input type="text" name="contrasena" placeholder="Ingrese Contrasena">
         <button type="submit">Guardar</button>
     </form>
 </body>
