fe77d5fe79a8b53f8419896eea1c64e21c2a7390
missaelcv <missaelcv0220@gmail.com>
2022-02-11 23:30:23 -0400

    encontrando la sentencia sql de usuario login

--- form-sesion/login.php
+++ form-sesion/login.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once '../yt_colores/conexion.php';
+
 $usuario_login = $_POST['nombre_usuario'];
 $contrasena_login = $_POST['contrasena'];
 
@@ -8,4 +10,12 @@ var_dump($usuario_login);
 var_dump($contrasena_login);
 echo '<pre>';
 
+//Verificar si Usuario Existe...
+$sql = 'SELECT * FROM usuarios WHERE nombre = ?';
+$sentencia = $pdo->prepare($sql);
+$sentencia->execute(array($$usuario_login));
+$resultado = $sentencia->fetch();
 
+echo '<pre>';
+var_dump($resultado);
+echo '<pre>';
